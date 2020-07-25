54395ca240696246a27f192f3d5886656dbaf639
John Slota <jslota17@comcast.net>
2020-07-24 23:39:57 -0500

    API get route

--- routes/api.js
+++ routes/api.js
@@ -1,2 +1,24 @@
 const app = require("express");
 const db = require("../models");
+
+//Post new workout
+app.post("/api/workouts", (req,res) => {
+    db.Workout.create({})
+     .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+  });
+
+//Get workouts
+app.get("/api/workouts", (req, res) => {
+    db.Workout.find({})
+      .then((dbWorkout) => { 
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+    });
